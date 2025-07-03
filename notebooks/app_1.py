024d84933e61ae158fdbb52af988cf67773b410c
Chelo MaGa <marcelomantillag@gmail.com>
2025-07-03 15:02:06 +0200

    Fix: Adjust data file path for Render deployement

--- notebooks/app_1.py
+++ notebooks/app_1.py
@@ -4,19 +4,18 @@ import plotly.express as px
 import streamlit as st
 
 try:
-    car_data = pd.read_csv("vehicles_us.csv")
+    car_data = pd.read_csv("notebooks/vehicles_us.csv")
 except FileNotFoundError:
     st.error("Error: El archivo 'vehicles_us.csv' no se encontró. "
-             "Asegúrate de que esté en la misma carpeta que 'app_1.py' o de que la ruta sea correcta.")
-    st.stop() # Detiene la ejecución si el archivo no se encuentra
-
+             )
+    st.stop() 
 
 st.header("Análisis de Datos de Anuncios de Venta de Vehículos")
 
 st.write("Haz clic en el botón para generar un histograma de la distribución del odómetro (kilometraje).")
-hist_button = st.button("Construir Histograma") # Crear un botón para el histograma
+hist_button = st.button("Construir Histograma") 
 
-if hist_button: # Si el botón del histograma es clicado
+if hist_button: 
 
     st.write("Creando un histograma para la distribución del odómetro (kilometraje).")
 
@@ -27,9 +26,8 @@ if hist_button: # Si el botón del histograma es clicado
 st.write("---")
 
 st.write("Haz clic en el botón para generar un gráfico de dispersión de precio vs. odómetro.")
-scatter_button = st.button("Construir Gráfico de Dispersión") # Crear otro botón para el gráfico de dispersión
-
-if scatter_button: # Si el botón del gráfico de dispersión es clicado
+scatter_button = st.button("Construir Gráfico de Dispersión") 
+if scatter_button: 
    
     st.write("Creando un gráfico de dispersión para explorar la relación entre precio y odómetro.")
 
